252790c3554901dd2ebe92e4cc351bf132d38ab4
viktorpavlovi4 <vlaritsky@mail.ru>
2022-07-13 13:25:11 +0300

    App init

---
+++ src/main/java/ru/javaops/bootjava/BootjavaApplication.java
@@ -0,0 +1,13 @@
+package ru.javaops.bootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javaops/bootjava/BootjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.bootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
